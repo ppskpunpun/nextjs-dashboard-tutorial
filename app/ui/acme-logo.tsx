c9626608ee93439cc0cf337407750936b576d449
Punpun <suracha2550@gmail.com>
2026-03-20 14:27:35 +0700

    try again

--- app/ui/acme-logo.tsx
+++ app/ui/acme-logo.tsx
@@ -1,13 +1,11 @@
 import { GlobeAltIcon } from '@heroicons/react/24/outline';
-// import { lusitana } from '@/app/ui/fonts';
-//
+import { lusitana } from '@/app/ui/fonts';
 
-// className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
 
 export default function AcmeLogo() {
   return (
     <div
-      className={` flex flex-row items-center leading-none text-white`}
+     className={`${lusitana.className} flex flex-row items-center leading-none text-white`}
     >
       <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
       <p className="text-[44px]">Acme</p>
